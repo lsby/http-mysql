c4e27706973476943aaa853b2e71bbf11ce8171f
Your Name <you@example.com>
2019-12-16 22:14:55 +0800

    .

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,24 @@
 var express = require('express')
 var router = express.Router()
+var mysql = require('mysql')
+var 扩展 = require('../node_api/index')
 
-router.post('/', function (req, res, next) {
-  res.send('respond with a resource')
+var mysqlConf = require('../conf/mysql')
+
+router.post('/query', async function (req, res, next) {
+    var sql = req.body.sql
+    if (sql == null || sql == '')
+        return res.send({ err: true, data: '无效的sql' })
+
+    var conn = mysql.createConnection(mysqlConf)
+    var { err, data } = await 扩展.其他工具.回调包装(back => conn.query(sql, (err, results, fields) => {
+        if (err) return back(err)
+        return back(null, { results, fields })
+    }))
+    return res.send({ err, data })
+})
+router.post('/exec', function (req, res, next) {
+    res.send('respond with a resource')
 })
 
 module.exports = router
