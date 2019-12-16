2fed40a686293a9fad6649a4cdffaab22b0125a1
Your Name <you@example.com>
2019-12-16 21:58:59 +0800

    .

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+router.post('/', function (req, res, next) {
+  res.send('respond with a resource')
 })
 
 module.exports = router
