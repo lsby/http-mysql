9519af899dcc5a899ee082b01ab45d315c71a132
lsby <2451759073@qq.com>
2019-12-17 00:55:54 +0800

    commit

---
+++ gitPush.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git config --global core.autocrlf false
+git config --global credential.helper store
+git config --global user.email "2451759073@qq.com"
+git config --global user.name "lsby"
+git config --global push.default simple
+git add --all
+git commit -m commit
+git push
