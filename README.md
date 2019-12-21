b42ff32696c6e6e76e4d5206526a360433852acb
lsby <2451759073@qq.com>
2019-12-21 05:08:07 +0000

    commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # http-mysql
- 使用http协议操作mysql数据库
+
+使用http协议操作mysql数据库
+
+## 启动
+
+### 本地
+
+```shell
+sh git-init-submodule.sh
+npm i
+node src/index.js
+
+# 要测试的话
+npm i -g mocha
+mocha
+```
+
+### docker
+
+```shell
+# 需要写好docker-compose.yml文件
+sh docker-rebuild.sh
+sh docker-restart.sh
+```
+
+## 使用
+
+- 使用`urlencoded`形式发送`post`请求到`/query`
+- 形式参考`test/test.test.js`文件和[mysql库](https://www.npmjs.com/package/mysql)
+
+## 配置
+
+通用:
+
+- conf/mysql.js:数据库相关,形式参考[mysql库](https://www.npmjs.com/package/mysql#pool-options)
+
+docker用:
+
+- conf/docker-use-cnpm:是否使用cnpm(文件存在即使用,下同)
+- conf/docker-use-rmLogOnStart:启动时是否删除日志文件
+- conf/docker-use-test:启动时是否运行测试
--- docker/run.sh
+++ docker/run.sh
@@ -5,18 +5,25 @@ echo '======================='
 echo '脚本开始'
 echo '======================='
 
+if [ -f '/root/code/conf/docker-use-cnpm' ];then
+    echo '使用cnpm'
+    if [ ! -f '/root/code/docker/installed-cnpm' ];then
+        npm install -g cnpm --registry=https://registry.npm.taobao.org
+        echo '' > /root/code/docker/installed-cnpm
+    fi
+    alias npm='cnpm'
+fi
 if [ ! -f '/root/code/docker/installed-npm-g' ];then
     echo '安装全局依赖'
-    npm install -g cnpm --registry=https://registry.npm.taobao.org
-    cnpm i -g mocha pm2
+    npm i -g pm2
     echo '' > /root/code/docker/installed-npm-g
 fi
 if [ ! -f '/root/code/docker/installed-npm' ];then
     echo '安装依赖'
-    cnpm i 
+    npm i 
     echo '' > /root/code/docker/installed-npm
 fi
-if [ -f '/root/code/docker/rmLogOnStart' ];then
+if [ -f '/root/code/conf/docker-use-rmLogOnStart' ];then
     echo '删除pm2日志'
     rm -rf /root/.pm2/*.log
     rm -rf /root/.pm2/logs
@@ -28,7 +35,13 @@ pm2 start src/index.js --name app
 echo '打印日志'
 pm2 logs &
 
-echo '单元测试'
-mocha
+if [ -f '/root/code/conf/docker-use-test' ];then
+    echo '使用测试'
+    if [ ! -f '/root/code/docker/installed-mocha' ];then
+        npm i -g mocha
+        echo '' > /root/code/docker/installed-mocha
+    fi
+    mocha
+fi
 
 tail -f /dev/null
